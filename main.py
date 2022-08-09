4b9bfb05ca8d66f67a80f846f6aca4f60d647a1f
sanjarbek <sanjarsaidov173@gamil.com>
2022-08-09 11:33:06 +0500

    done

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def requesting_multiple_users(n:int)->list:
+    # url for random user
+    url = f'https://randomuser.me/api/?results={n}'
+    # request
+    r = requests.get(url) 
+    #check status code
+    if r.status_code == 200:
+        # returs list cotainer n  users
+        return r.json()['results']
+    else:
+        return []
+
+if __name__ == '__main__':
+    print(requesting_multiple_users(3))
